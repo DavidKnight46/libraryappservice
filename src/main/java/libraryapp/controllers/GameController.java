8d1c74774a8a633b5065eb8eb3659dccf339242b
David Knight <disco.david.85@gmail.com>
2021-10-17 21:05:21 +0100

    Design added

--- src/main/java/libraryapp/controllers/GameController.java
+++ src/main/java/libraryapp/controllers/GameController.java
@@ -1,9 +1,7 @@
 package libraryapp.controllers;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 public interface GameController {
