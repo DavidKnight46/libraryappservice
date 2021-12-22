b770f838742e256a06aafad02d36d48e65bde9a5
David Knight <disco.david.85@gmail.com>
2021-12-22 18:36:54 +0000

    Further v2 implementation

--- src/main/java/libraryapp/service/games/gameV2/GameServiceV2Impl.java
+++ src/main/java/libraryapp/service/games/gameV2/GameServiceV2Impl.java
@@ -2,9 +2,7 @@ package libraryapp.service.games.gameV2;
 
 import libraryapp.api.ApiClient;
 import libraryapp.api.ApiClientRawImpl;
-import libraryapp.configuration.RawConfiguration;
 import libraryapp.models.modelsV2.GameModelV2;
-import libraryapp.repository.GameRepository;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -12,15 +10,9 @@ import java.io.IOException;
 @Service
 public class GameServiceV2Impl implements GameServiceV2 {
 
-    private final RawConfiguration rawConfiguration;
-    private final GameRepository gameRepository;
     private final ApiClient apiClient;
 
-    public GameServiceV2Impl(RawConfiguration rawConfiguration,
-                             GameRepository gameRepository,
-                             ApiClientRawImpl apiClient) {
-        this.rawConfiguration = rawConfiguration;
-        this.gameRepository = gameRepository;
+    public GameServiceV2Impl(ApiClientRawImpl apiClient) {
         this.apiClient = apiClient;
     }
 
